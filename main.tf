eabdbb798531af6a4ee50dd87af1bad914f0dec5
VishnuPradeep18 <113281667+VishnuPradeep18@users.noreply.github.com>
2025-09-30 09:16:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-08982f1c5bf93d976"
 instance_type = "t3.micro"
+vpc_security_group_ids = ["sg-07bf17e2985332647"]
 tags = {
 Name = "Test-server"
 }
