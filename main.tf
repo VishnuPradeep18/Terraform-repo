008904cb5ba87bc436cc1619d96167ef2a87de10
VishnuPradeep18 <113281667+VishnuPradeep18@users.noreply.github.com>
2025-10-01 16:40:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
+count = 4
 ami = "ami-08982f1c5bf93d976"
 instance_type = "t3.micro"
 vpc_security_group_ids = ["sg-07bf17e2985332647"]
